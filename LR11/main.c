a11bee4ce4524825ff8d61ca7d40f0c024645744
ilyaTheCreator <respondatanytime@gmail.com>
2022-06-09 20:14:38 +0300

    task 2

--- LR11/main.c
+++ LR11/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 Z1()
 {
@@ -16,9 +17,50 @@ Z1()
     fclose(fp);
 }
 
+Z2()
+{
+    struct Participant
+    {
+        char nick[50];
+        int score;
+    };
+
+    FILE *fp;
+    struct Participant participants[6];
+    int i;
+
+    for (i = 0; i < 6; i++)
+    {
+        struct Participant participant;
+
+        printf("\n  Participant[%d] nick  : ", i + 1);
+        scanf("%[^\n]%*c", participants[i].nick);
+
+        if (strlen(participants[i].nick) > 8)
+        {
+            printf("\n Participant's nick should have less than 8 characters \n");
+            exit(0);
+        }
+
+        printf("  Participant[%d] score : ", i + 1);
+        scanf("%d%*c", &participants[i].score);
+    }
+
+    fp = fopen("./file2.txt", "w");
+
+    for (i = 0; i < 6; i++)
+    {
+        fprintf(fp, "Person %d nick: %s \n", i + 1, participants[i].nick);
+        fprintf(fp, "Person %d score: %d \n \n", i + 1, participants[i].score);
+    }
+
+    fclose(fp);
+}
+
 int main()
 {
     //Z1();
+    Z2();
 
     return 0;
 }
