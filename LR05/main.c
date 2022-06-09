463347e38991a94bce2209aa13a19d3e4660f154
Bill44-NotAlone <denyc9@mail.ru>
2022-06-09 15:56:52 +0300

    Задание 3

--- LR05/main.c
+++ LR05/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <string.h>
 
 void Z1_1()
 {
@@ -18,8 +19,25 @@ void Z1_2()
         printf("%c - %i\n", i, i);
 }
 
+void Z3()
+{
+    char str1[100];
+    char str2[100];
+    char str3[100];
+    char str4[1000];
+    scanf("%s %s %s", str1, str2, str3);
+    str1[0] = toupper(str1[0]);
+    str2[0] = toupper(str2[0]);
+    str3[0] = toupper(str3[0]);
+    strcat(str4, str1);
+    strcat(str4, str2);
+    strcat(str4, str3);
+    printf("%s", str4);
+}
+
 int main()
 {
     //Z1_1();
     //Z1_2();
+    //Z3();
 }
