be694d539639335814b01f106fa879a3672e47b0
ilyaTheCreator <respondatanytime@gmail.com>
2022-06-09 19:05:18 +0300

    task 1

--- LR10/main.c
+++ LR10/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void Z1()
+{
+char contents[1000];
+
+ //Opening the file
+ FILE * fp;
+ fp = fopen("./file1.txt", "w"); //"w" = write
+
+ //If there is an error
+ if(fp == NULL)
+{
+    //Exit
+    printf("Error!\n");
+    exit(EXIT_FAILURE);
+}
+
+//This part require your input
+printf("Enter the contents of file: \n");
+fgets(contents, 1000, stdin);
+
+
+//Write your input in file
+fputs(contents, fp);
+
+//Close the file
+fclose(fp);
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    Z1();
+
     return 0;
 }
