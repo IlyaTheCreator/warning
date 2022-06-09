b78cde008cd432e99baf1d7a5ec9a1799ea85459
Bill44-NotAlone <denyc9@mail.ru>
2022-06-09 17:04:27 +0300

    Задание 1

--- LR08/main.c
+++ LR08/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Product
+{
+    char name[100];
+    int count;
+    float price;
+}product;
+
+void Z1()
+{
+    scanf("%s", product.name);
+    scanf("%i", &product.count);
+    scanf("%f", &product.price);
+    printf("%s %f", product.name, product.count * product.price);
+}
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    //Z1();
 }
