403328f12ba2f01cc188b6efca6321284ffa96b0
Bill44-NotAlone <denyc9@mail.ru>
2022-06-09 15:31:47 +0300

    1 задание

--- LR05/main.c
+++ LR05/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+
+void Z1_1()
+{
+    system("chcp 1251");
+    for(int i = 224; i < 256; i ++)
+        printf("%c - %i\n", i, i);
+}
+
+void Z1_2()
+{
+    system("chcp 866");
+    for(int i = 160; i < 176; i ++)
+        printf("%c - %i\n", i, i);
+    for(int i = 224; i < 240; i ++)
+        printf("%c - %i\n", i, i);
+}
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    //Z1_1();
+    //Z1_2();
 }
