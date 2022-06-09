c8f400c6407640f9cc81cfc3da9306062255954d
ilyaTheCreator <respondatanytime@gmail.com>
2022-06-09 17:27:13 +0300

    task 2

--- LR08/main.c
+++ LR08/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void Z2()
+{
+    struct Person {
+       char second_name[50];
+       int  height;
+    };
+
+    struct Person people[5];
+    int i;
+    int average_height = 0;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("\n  Person[%d] second name  : ", i + 1);
+        scanf("%[^\n]%*c", people[i].second_name);
+        printf("  Person[%d] height : ", i + 1);
+        scanf("%d%*c", &people[i].height);
+
+        average_height += people[i].height;
+    }
+
+    average_height = average_height / 5;
+
+    printf("\n Average height: %d \n", average_height);
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    Z2();
+
     return 0;
 }
