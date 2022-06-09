3001b381076318198ed0a869ddc68cf35f4997c7
Bill44-NotAlone <denyc9@mail.ru>
2022-06-09 17:22:08 +0300

    Задание 3 (не локализовано)

--- LR08/main.c
+++ LR08/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <windows.h>
 
 struct Product
 {
@@ -16,7 +18,17 @@ void Z1()
     printf("%s %f", product.name, product.count * product.price);
 }
 
+void Z3()
+{
+    struct tm * timeinfo;
+    time_t rawtime;
+    time( &rawtime );
+    timeinfo = localtime( &rawtime );
+    printf("%s", asctime(timeinfo));
+}
+
 int main()
 {
     //Z1();
+    //Z3();
 }
